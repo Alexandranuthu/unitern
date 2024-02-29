ee7e4d99d1510dcfa2807b7ef89b9a7cfb93968c
Alexandranuthu <alexandranuthuu@gmail.com>
2024-02-29 14:44:06 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Welcome to Alumnix</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <nav class="nav">
+                <li>Find Jobs</li>
+                <li>Find Developers</li>
+                <li>Portfolio</li>
+            </nav>
+        </header>
+    </div>
+</body>
+</html>
